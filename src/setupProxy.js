3e62819f9a544060d4fd1e8be99cfa0c15c289d3
Lucioric2000 <lucioric2000@hotmail.com>
2024-03-15 00:00:26 -0600

    Added CORS to the proxyy for the backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,6 +8,11 @@ module.exports = function(app) {
       pathRewrite: {'^/api' : ''}, //so that the requests in the backend doesn't have /api prefix
       target: 'http://localhost:8000',
       changeOrigin: true,
+      onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        proxyRes.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
+      }
     })
   );
 }
