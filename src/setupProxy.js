5fc0491e0c4ccfac4ed46d54c12be4a96781d7d4
Lucioric2000 <lucioric2000@hotmail.com>
2024-03-17 00:51:59 -0600

    Quitted the proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 //import {backend_netloc} from './utils/constants';
 
 module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use(
     '/simulation',
     createProxyMiddleware({
-      pathRewrite: {'^/api' : ''}, //so that the requests in the backend doesn't have /api prefix
+      //pathRewrite: {'^/api' : ''}, //so that the requests in the backend doesn't have /api prefix
       target: 'http://localhost:8000',
       changeOrigin: true,
       onProxyRes: function (proxyRes, req, res) {
